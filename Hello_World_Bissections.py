1003fece0cab2a7d8127426d3dabfaf31a5c9c9c
Lucas Motta Freire <mottalf@ime.usp.br>
2020-03-09 20:50:46 -0300

    adicionado a função media

--- Hello_World_Bissections.py
+++ Hello_World_Bissections.py
@@ -1,3 +1,12 @@
 # Este é o meu primeiro arquivo utilizando o GIT
 
 # Tenho que elaborar um método para encontrar as raízes de uma equação do tipo f(X)=0
+
+a = 1
+b = 2
+
+def media(a0, b0):
+    return (a0 + b0)/2
+
+print(media(1, 2))
+
