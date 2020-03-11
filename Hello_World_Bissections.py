a29885232f733ec26afff6a8464a386155e49d83
Lucas Motta Freire <mottalf@ime.usp.br>
2020-03-11 14:09:40 -0300

    Terminado o código para o método da bisseção

--- Hello_World_Bissections.py
+++ Hello_World_Bissections.py
@@ -2,11 +2,33 @@
 
 # Tenho que elaborar um método para encontrar as raízes de uma equação do tipo f(X)=0
 
+#Importar a biblioteca matemática
+
+import math
+
+# Definir um intervalo [a,b]
 a = 1
 b = 2
+e=0.00001
 
-def media(a0, b0):
-    return (a0 + b0)/2
+#Definir uma função
 
-print(media(1, 2))
+def f(x):
+    return x**2-2
+
+# Teste do teorema de Bolzano
+if f(a)*f(b)<0:
+    while (math.fabs(b-a)/2 > e):    # Criando um laço
+        xi=(a+b)/2                   
+        if f(xi) == 0:
+            print("A Raiz é:" ,xi)
+            break
+        else:
+            if f(a)*f(xi) < 0:
+                b = xi
+            else:
+                a = xi
+    print("Valor da raiz é:", xi)
+else:
+    print("não há raiz neste intervalo")
 
